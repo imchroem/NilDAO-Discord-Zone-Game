c47d1833045960fc9346952ba241b51c8aa7ff7a
seb <118224616+keilby@users.noreply.github.com>
2022-11-15 20:18:14 +0100

    Uploaded.

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "OTQwNjgzOTg0Mjk0MjUyNTU0.YgK-Og.8Oi4p8WFHxl4SgJkEJW3wFyOmtI",
+    prefix: "-",
+    mongodb: "mongodb+srv://Seb:p9gwJg61uXfJkce8@cluster0.kbxbd.mongodb.net/NIL9000?retryWrites=true&w=majority",
+    guildID: "884489381510021152",
+    announcements: "939872422709059655",
+    time: 36000000, // HOURS * MINS * SECONDS * MSECONDS (TIME IN MILESECONDS)
+    targetNumber: 333, // TARGET NUMBERS
+    delayTime: 36000000, // HOURS * MINS * SECONDS * MSECONDS (TIME IN MILESECONDS)
+    GameChannel: "940682846081806486" // GAME CHANNEL ID
+}
